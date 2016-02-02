a1ccd0dda474ebdb5e48775b582c79c6bd781e7f
sriv <srikanth.ddit@gmail.com>
2016-02-02 10:50:47 +0530

    add missing file

---
+++ Implementation/DriverExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Gauge.Example.Implementation
+{
+    public static class DriverExtensions
+    {
+        public static void Visit(this IWebDriver driver, string url)
+        {
+            driver.Navigate().GoToUrl(url);
+            new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(ExpectedConditions.ElementExists(By.TagName("body")));
+        }
+    }
+}
